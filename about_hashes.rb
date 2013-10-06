9ca8569c22dc673c2820d364dd5d9afc09390e46
xorrr <derxor@gmail.com>
2013-10-06 22:59:28 +0200

    pass test_accessing_hashes_with_fetch and add comment

--- about_hashes.rb
+++ about_hashes.rb
@@ -22,14 +22,17 @@ class AboutHashes < Neo::Koan
 
   def test_accessing_hashes_with_fetch
     hash = { :one => "uno" }
-    assert_equal __, hash.fetch(:one)
-    assert_raise(___) do
+    assert_equal "uno", hash.fetch(:one)
+    assert_raise(KeyError) do
       hash.fetch(:doesnt_exist)
     end
 
     # THINK ABOUT IT:
     #
     # Why might you want to use #fetch instead of #[] when accessing hash keys?
+
+    # ANSWER:
+    # I get a key error instead of just nil
   end
 
   def test_changing_hashes
